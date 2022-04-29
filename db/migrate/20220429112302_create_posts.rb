f5deee9f6b7a201006ea2547a389b782d48603af
Mithi-c0de <mithlesh.9m@gmail.com.com>
2022-04-29 17:47:51 +0530

    fix foreign keys

--- db/migrate/20220429112302_create_posts.rb
+++ db/migrate/20220429112302_create_posts.rb
@@ -5,7 +5,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
       t.text :text
       t.text :comments_counter
       t.text :likes_counter
-      t.references :author, foregin_key: { to_table: 'users' }, index: true
+      t.references :author, foreign_key: { to_table: 'users' }, index: true
 
       t.timestamps
     end
--- db/migrate/20220429112416_create_comments.rb
+++ db/migrate/20220429112416_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.text :text
-      t.references :author, foregin_key: { to_table: 'users' }, index:true
+      t.references :author, foreign_key: { to_table: 'users' }, index:true
       t.references :post, foreign_key: { to_table: 'posts' }, index:true
 
       t.timestamps
