f95e45952ea9814a50b87dc8b4ad5a9aaeb6e56f
Mithi-c0de <mithlesh.9m@gmail.com.com>
2022-05-02 08:19:23 +0530

    updated forgien key

--- db/migrate/20220502024230_create_posts.rb
+++ db/migrate/20220502024230_create_posts.rb
@@ -3,8 +3,8 @@ class CreatePosts < ActiveRecord::Migration[7.0]
     create_table :posts do |t|
       t.string :title
       t.text :text
-      t.text :comments_counter
       t.text :likes_counter
+      t.text :comments_conter
       t.references :author, foreign_key: { to_table: 'users' }, index: true
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_29_112500) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_02_024516) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,8 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_29_112500) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "text"
-    t.text "comments_counter"
     t.text "likes_counter"
+    t.text "comments_conter"
     t.bigint "author_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -54,5 +54,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_29_112500) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users", column: "author_id"
   add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users", column: "author_id"
+  add_foreign_key "posts", "users", column: "author_id"
 end
