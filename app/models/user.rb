152dbff714413c45124ea642f362b355b0e65f4f
Mithi-c0de <mithlesh.9m@gmail.com.com>
2022-05-03 17:15:56 +0530

    fix linter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :posts, class_name: 'post', foreign_key: 'author_id'
   has_many :likes, class_name: 'like', foreign_key: 'author_id'
 
-    # A method that returns the 3 most recent posts for a given user.
+  # A method that returns the 3 most recent posts for a given user.
   def latest_posts
     posts.limit(3).order(created_at: :desc)
   end
