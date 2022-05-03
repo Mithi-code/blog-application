45880d975e8fe85891d59e386f8d5648231a4a6f
Mithi-c0de <mithlesh.9m@gmail.com.com>
2022-05-03 16:31:05 +0530

    added associations to models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,10 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :post
+  belongs_to :author, class_name: 'User'
+  belongs_to :post, class_name: 'Post'
+  after_save :update_comments_counter
+
+  # A method that updates the comment counter for a post.
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,16 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  belongs_to :author, class_name: 'User'
+  has_many :likes, class_name: 'like', foreign_key: 'post_id'
+  has_many :comments, class_name: 'comment', foreign_key: 'post_id'
+  after_save :update_posts_counter
+
+  # A method that updates the posts counter for a user.
+  def update_posts_counter
+    author.increment!(:posts_counter)
+  end
+
+  # Method which returns the 5 most recent comments for a given post.
+  def latest_posts
+    comments.limit(5).order(created_at: :desc)
+  end
 end
