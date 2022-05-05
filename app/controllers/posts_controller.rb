80438724b61e44bf9bb852c3eb8a5b4608996b48
Mithi-c0de <mithlesh.9m@gmail.com.com>
2022-05-05 09:31:30 +0530

    fixed linter error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,7 @@
 class PostsController < ApplicationController
   def index
-  @post = Post.all
-  @user = User.find(params[:user_id])
+    @post = Post.all
+    @user = User.find(params[:user_id])
   end
 
   def show
