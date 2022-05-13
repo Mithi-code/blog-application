9208c10f1540664f7ac2b4b5ebd2dddd9942b070
brytebee <brytebee@gmail.com>
2022-05-13 20:06:04 +0100

    :white_check_mark: Update comments_controller.rb
    Added index action

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,2 +1,9 @@
 class Api::CommentsController < ApplicationController
+  protect_from_forgery with: :null_session
+  before_action :authorize_request
+
+  def index
+    comments = Comment.where(post_id: params[:post_id])
+    json_response(comments)
+  end
 end
